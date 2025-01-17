f26312ab3565173972b2f4a6f12c4d1ea7bdcc52
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-17 12:29:18 -0300

    feat: add fetch data function for auctions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <h1 className="text-3xl font-bold">Car Shop Course</h1>;
+import AuctionCard from "../components/AuctionCard";
+
+async function getData(): Promise<unknown> {
+  const response = await fetch("http://localhost:6001/search");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return response.json();
+}
+
+export default async function Home() {
+  const data = await getData();
+
+  return (
+    <div className="grid grid-cols-4 gap-6">
+      {data &&
+        data.results.map((result: any) => (
+          <AuctionCard key={result.id} {...result} />
+        ))}
+    </div>
+  );
 }
