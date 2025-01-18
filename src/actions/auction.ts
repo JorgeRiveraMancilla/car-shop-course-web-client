4b5a9a07c440f8ab458bad84f302889883782221
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-18 19:19:07 -0300

    refactor: simplify api query string handling

--- src/actions/auction.ts
+++ src/actions/auction.ts
@@ -3,13 +3,8 @@
 import { Auction } from "@/models/Auction";
 import { PagedResult } from "@/models/PagedResult";
 
-export async function getData(
-  pageNumber: number = 1,
-  pageSize: number = 8
-): Promise<PagedResult<Auction>> {
-  const response = await fetch(
-    `http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`
-  );
+export async function getData(query: string): Promise<PagedResult<Auction>> {
+  const response = await fetch(`http://localhost:6001/search${query}`);
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
