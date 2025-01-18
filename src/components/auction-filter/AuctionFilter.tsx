f5e57250356ffcf3f91399aafbd76ec9d6723e96
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-18 20:55:06 -0300

    feat: add auction status filtering

--- src/components/auction-filter/AuctionFilter.tsx
+++ src/components/auction-filter/AuctionFilter.tsx
@@ -9,8 +9,9 @@ import {
 } from "@/components/ui/select";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import { AiOutlineClockCircle, AiOutlineSortAscending } from "react-icons/ai";
-import { BsTagFill } from "react-icons/bs";
+import { BsStopwatchFill, BsTagFill } from "react-icons/bs";
 import { Button } from "../ui/button";
+import { GiFinishLine, GiFlame } from "react-icons/gi";
 
 const pageSizeOptions = [4, 8, 12, 16];
 const orderButtons = [
@@ -20,14 +21,31 @@ const orderButtons = [
     value: "make",
   },
   {
-    label: "Finaliza pronto",
+    label: "Tiempo",
     icon: AiOutlineClockCircle,
     value: "endingSoon",
   },
   {
-    label: "Nuevos",
+    label: "Año",
     icon: BsTagFill,
     value: "new",
+  },
+];
+const filterButtons = [
+  {
+    label: "Disponibles",
+    icon: GiFlame,
+    value: "live",
+  },
+  {
+    label: "Finaliza pronto",
+    icon: GiFinishLine,
+    value: "endingSoon",
+  },
+  {
+    label: "Terminados",
+    icon: BsStopwatchFill,
+    value: "finished",
   },
 ];
 
@@ -35,17 +53,37 @@ export default function AuctionFilter() {
   const pageSize = useParamsStore((state) => state.pageSize);
   const setParams = useParamsStore((state) => state.setParams);
   const orderBy = useParamsStore((state) => state.orderBy);
+  const filterBy = useParamsStore((state) => state.filterBy);
 
   return (
     <div className="flex items-center justify-between mb-4">
       <div className="flex items-center gap-2">
         <span className="text-sm text-muted-foreground mr-2">Ordenar por:</span>
+
         <div className="flex gap-2 flex-wrap">
           {orderButtons.map(({ label, icon: Icon, value }) => (
             <Button
               key={value}
               onClick={() => setParams({ orderBy: value })}
               variant={orderBy === value ? "default" : "outline"}
+              className="flex items-center gap-2"
+            >
+              <Icon className="h-4 w-4" />
+              {label}
+            </Button>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <span className="text-sm text-muted-foreground mr-2">Filtrar por:</span>
+
+        <div className="flex gap-2 flex-wrap">
+          {filterButtons.map(({ label, icon: Icon, value }) => (
+            <Button
+              key={value}
+              onClick={() => setParams({ filterBy: value })}
+              variant={filterBy === value ? "default" : "outline"}
               className="flex items-center gap-2"
             >
               <Icon className="h-4 w-4" />
