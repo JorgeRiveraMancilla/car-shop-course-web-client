8f193133317c2dcda95892b6bbe9e3dca85460dc
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-01-19 15:58:21 -0300

    fix: update import paths to lowercase

--- src/actions/auction.ts
+++ src/actions/auction.ts
@@ -1,7 +1,7 @@
 "use server";
 
-import { Auction } from "@/models/Auction";
-import { PagedResult } from "@/models/PagedResult";
+import { Auction } from "@/models/auction";
+import { PagedResult } from "@/models/pagedResult";
 
 export async function getData(query: string): Promise<PagedResult<Auction>> {
   const response = await fetch(`http://localhost:6001/search${query}`);
--- src/components/auction-card/AuctionCard.tsx
+++ src/components/auction-card/AuctionCard.tsx
@@ -1,4 +1,4 @@
-import { Auction } from "@/models/Auction";
+import { Auction } from "@/models/auction";
 import CarImage from "./CarImage";
 import CountdownTimer from "./CountdownTimer";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
