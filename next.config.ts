1cb40ef73f06177d78ac265d932803289e718700
JorgeRiveraMancilla <jiriveramancilla@gmail.com>
2025-05-30 19:31:03 -0400

    fix: api paths

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,16 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/:path*",
-        destination: "http://localhost:6001/:path*",
+        source: "/api/auctions/:path*",
+        destination: "http://localhost:6001/auctions/:path*",
+      },
+      {
+        source: "/api/search/:path*",
+        destination: "http://localhost:6001/search/:path*",
+      },
+      {
+        source: "/api/bids/:path*",
+        destination: "http://localhost:6001/bids/:path*",
       },
     ];
   },
